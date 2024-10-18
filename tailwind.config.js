f06120cbca362384a99bf957d9198a7f71ba3212
Tyler Hughes <tyler@tylerhughes.info>
2024-10-17 22:13:35 -0500

    Updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./styles/globals.css",
+    "./node_modules/@shadcn/ui/dist/**/*.js",
   ],
   theme: {
     extend: {
@@ -57,12 +58,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    // Light & dark themes are added by default (it switches automatically based on OS settings)
-    // You can add another theme among the list of 30+
-    // Add "data-theme='theme_name" to any HTML tag to enable the 'theme_name' theme.
-    // https://daisyui.com/
-    themes: ["light", "dark"],
-  },
+  plugins: [
+    require("tailwindcss-radix"),
+  ],
 };
