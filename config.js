5eeb56f1ff8d1128fb7fc1a1d05e70312d7b21f3
Tyler Hughes <tyler@tylerhughes.info>
2024-10-19 21:16:57 -0500

    Partial migration from daisyUI to shadcn/UI

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import themes from "daisyui/src/theming/themes";
-
 const config = {
   // REQUIRED
   appName: "ClickTrackr",
@@ -7,13 +5,7 @@ const config = {
   appDescription:
     "ClickTrackr is an intuitive tool for tracking, managing, and optimizing affiliate links to boost your revenue. Enhance your affiliate marketing efficiency with robust analytics and seamless integration.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "clicktrackr.io",
-  crisp: {
-    // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
-    id: "",
-    // Hide Crisp by default, except on route "/". Crisp is toggled with <ButtonSupport/>. If you want to show Crisp on every routes, just remove this below
-    onlyShowOnRoutes: ["/"],
-  },
+  domainName: "tryclicktrackr.com",
   stripe: {
     // Used to determine what text to say i.e. /month vs /year vs USD
     mode: 'subscription',
@@ -21,18 +13,13 @@ const config = {
     plans: [
       {
         isFeatured: false,
-        // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
         priceId:
           process.env.NODE_ENV === "development"
             ? "price_1Niyy5AxyNprDp7iZIqEyD2h"
             : "price_456",
-        //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Essentials",
-        // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
         description: "Designed for solo entrepreneurs",
-        // The price you want to display, the one user will be charged on Stripe.
         price: 19,
-        // If you have an anchor price (i.e. $29) that you want to display crossed out, put it here. Otherwise, leave it empty
         priceAnchor: 39,
         annualPrice: 190,
         annualPriceAnchor: 228,
@@ -44,7 +31,6 @@ const config = {
         ],
       },
       {
-        // This plan will look different on the pricing page, it will be highlighted. You can only have one plan with isFeatured: true
         isFeatured: true,
         priceId:
           process.env.NODE_ENV === "development"
@@ -61,8 +47,6 @@ const config = {
           { name: "Everything in Essentials Plan" },
           { name: "Up to 3 user accounts" },
           { name: "Priority Email Support" },
-          // { name: "Custom Domains for Link Shortening" },
-          // { name: "Integration with Major Affiliate Networks" },
         ],
       },
       {
@@ -80,40 +64,29 @@ const config = {
         features: [
           { name: "Unlimited affiliate links" },
           { name: "Everything in Professional Plan" },
-          { name: "Unlimited user accounts" }
+          { name: "Unlimited user accounts" },
         ],
-      }
+      },
     ],
   },
   aws: {
-    // If you use AWS S3/Cloudfront, put values in here
     bucket: "bucket-name",
     bucketUrl: `https://bucket-name.s3.amazonaws.com/`,
     cdn: "https://cdn-id.cloudfront.net/",
   },
   mailgun: {
-    // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
-    // REQUIRED — Email 'From' field to be used when sending magic login links
     fromNoReply: `ShipFast <noreply@mg.shipfa.st>`,
-    // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
     fromAdmin: `Marc at ShipFast <marc@mg.shipfa.st>`,
-    // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
     supportEmail: "marc@mg.shipfa.st",
-    // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
     forwardRepliesTo: "marc.louvion@gmail.com",
   },
   colors: {
-    // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
-    theme: "dark",
-    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
-    // OR you can just do this to use a custom color: main: "#f37055". HEX only.
-    main: themes["dark"]["primary"],
+    theme: "dark",  // You can keep this or customize it in your Tailwind setup
+    main: "#f37055",  // Replace with your custom primary color
   },
   auth: {
-    // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
     loginUrl: "/api/auth/signin",
-    // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
     callbackUrl: "/dashboard",
   },
 };
