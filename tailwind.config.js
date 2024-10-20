b0f1656253a2a44ad73778f11f06d9fc18440d20
Tyler Hughes <tyler@tylerhughes.info>
2024-10-20 12:36:37 -0500

    Minor change to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-   './pages/**/*.{js,ts,jsx,tsx}',
-   './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@shadcn/ui/dist/**/*.{js,ts,jsx,tsx}'
 	],
   theme: {
     container: {
